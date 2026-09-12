ff9477fac0cb79a9a109ecb628c09ef771627b20
kevindesantis <kevindavide31@gmail.com>
2026-09-12 17:04:19 +0200

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,5 @@
+
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://tmsnbfcffegetuklriuc.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtc25iZmNmZmVnZXR1a2xyaXVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMTgxODgsImV4cCI6MjEwNDc5NDE4OH0.IICQXc0wNWHOc-n5ZSMSHpzyyDixnhixzcmc4naCfRc"
+};
